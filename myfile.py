8bcf49b970ee1023af1b84dc0b9cb478b1a8a5d4
DavyNerdJones <topa.georg@gmail.com>
2026-08-20 10:13:44 +0300

    add mod 3ex

---
+++ myfile.py
@@ -0,0 +1,24 @@
+print("Hello")
+
+print('"Hello" , \nsaid Dave')
+
+print ('"I\'m Dave"')
+
+
+print (5)
+print (10)
+
+
+name = input('Enter your name: ')
+print("Nice to meet you, " + name + "!")
+
+
+points = 50  # points is now 50
+print(points)  # prints: 50
+
+points = 120  # now points is 120
+print(points)  # prints: 120
+
+#Error to the bottom lines
+#player_name = Geeeeorge
+#print ("+player_name")
