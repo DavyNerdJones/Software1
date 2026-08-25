5a9fbe650be68bb314f9eb5cabe2ca614eb1f2ad
DavyNerdJones <topa.georg@gmail.com>
2026-08-25 12:49:00 +0300

    Completed exercise4

--- Module 3/exercise4.py
+++ Module 3/exercise4.py
@@ -2,16 +2,15 @@ num1 = input ("Enter the first integer: ")
 num2 = input ("Enter the second integer: ")
 num3 = input ("Enter the third integer: ")
 
-num1 =float(first integer)
-num2 =float(second integer)
-num3 =float(third integer)
+num1 =float(num1)
+num2 =float(num2)
+num3 =float(num3)
 
-sum =
-product =
-average =
+sum = ( num1 + num2 + num3 )
+product = (num1 * num2* num3)
+average = sum/3
 
-
-print"The sum of the numbers: " + str(sum)
-print"The product of the numbers: " + str(product)
-print"The average of the numbers: " + str(average)
+print("The sum of the numbers: " + str(sum))
+print("the product of the numbers: " + str(product))
+print("The average of the numbers : " +str(average))
 
--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 
 **My name is Georgios Topalidis**
 
-## Module 1
-I did exercises 1,2,3
+## Module 1&2
+I did exercise 1
 
-## Module 2
-I did exeercises 2,3,4,5,6
+## Module 3
+I did exercises 1,2,3
