eaf2589f2975d68049c19c45703a2bba0e280d4b
DavyNerdJones <topa.georg@gmail.com>
2026-08-27 10:32:23 +0300

    Completed Module 4

--- Module 4/Exercise3.py
+++ Module 4/Exercise3.py
@@ -1,2 +1,19 @@
-Enter biological gender (male/female):
-Enter hemoglobin value (g/l):
+gender =input("Enter biological gender (male/female): ")
+hemoglobin_value =float(input("Enter hemoglobin value (g/l):"))
+
+gender = gender.lower()
+
+if gender == "male" and (134 <= hemoglobin_value and hemoglobin_value <= 167):
+    print(" Your hemoglobin is normal.")
+elif gender == "male" and (167 < hemoglobin_value):
+    print(" Your hemoglobin is high.")
+elif gender == "male" and (hemoglobin_value < 134):
+    print(" Your hemoglobin is low.")
+elif gender =="female" and (117 <= hemoglobin_value and hemoglobin_value <= 155):
+    print(" Your hemoglobin is normal.")
+elif gender =="female" and (hemoglobin_value < 134):
+    print(" Your hemoglobin is low.")
+elif gender =="female" and (hemoglobin_value > 167):
+    print(" Your hemoglobin is high.")
+if (gender!= "male" and gender != "female" ):
+    print("invalid gender")
---
+++ Module 4/Exercise4.py
@@ -0,0 +1,15 @@
+year = input("Enter a year" )
+
+year =int(year)
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) :
+    print (f"{year} is a leap year.")
+
+else:
+    print (f"{year} is not a leap year.")
+
+
+
+
+
+
