95695e3d0566f56245001769adf6e0f47eff04b4
DavyNerdJones <topa.georg@gmail.com>
2026-08-30 20:36:16 +0300

    Tried to match it with Moodle, no success

--- TESTs N Stuffs/teeeest.py
+++ TESTs N Stuffs/teeeest.py
@@ -2,10 +2,10 @@ cabin_class = "Enter the cabin class (LUX, A, B, or C): "
 selection = input(cabin_class)
 
 if selection =="LUX":
-     print(f"Upper-deck cabin with a balcony.")
+        print(f"Upper-deck cabin with a balcony.")
 
 elif selection =="A":
-      print(f"Above the car deck, equipped with a window.")
+        print(f"Above the car deck, equipped with a window.")
 
 elif selection =="B":
         print(f"Windowless cabin above the car deck.")
@@ -14,4 +14,4 @@ elif selection =="C":
         print(f"Windowless cabin below the car deck.")
         
 elif selection != cabin_class:
-      print(f"Invalid cabin class.")
+    print(f"Invalid cabin class.")
