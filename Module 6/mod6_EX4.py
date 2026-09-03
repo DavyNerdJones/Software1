47839c24375ab918240ed85ee325040f1afe9352
DavyNerdJones <topa.georg@gmail.com>
2026-09-03 13:33:47 +0300

    completed mod6 exercises

---
+++ Module 6/mod6_EX4.py
@@ -0,0 +1,13 @@
+cities = []
+
+city = str(input("Enter the name of a city: "))
+for c in range(5):
+    cities.append(str(city))
+    city = str(input("Enter the name of a city: "))
+print ("The cities you entered: ")
+for city in cities:
+    print (city)
+
+
+
+#To check with moodle again. No idea why
