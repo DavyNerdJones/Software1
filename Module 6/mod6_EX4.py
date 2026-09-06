3a00cad52a8f17a6dfab2f00da145144519cee2b
DavyNerdJones <topa.georg@gmail.com>
2026-09-06 15:21:53 +0300

    fixed some stuff to match with Moodle and updated readme file

--- Module 6/mod6_EX4.py
+++ Module 6/mod6_EX4.py
@@ -5,10 +5,12 @@ for c in range(5):
     city = input("Enter the name of a city: ")
     cities.append(str(city))
 
-print ("The cities you entered: ")
+print ("\n\nThe cities you entered: ")
 for city in cities:
     print (city)
 
 
 
 #To check with moodle again. No idea why
+
+#Note to self,  "  \n\n for 2 "enter spaces"  "
--- readme.md
+++ readme.md
@@ -7,3 +7,15 @@ I did exercise 1
 
 ## Module 3
 I did exercises 1,2,3,4,5,6
+
+## Module 4
+I did exercises 1,2,3,4
+
+## Module 5
+I did exercises 1,2,3,4,5,6,
+
+## Module 6
+I did exercises 1,2,3,4,5
+
+## Module 7
+I did exercises 1,2,3,4,5,6
